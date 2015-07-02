1ffb5f6a5fc8590d86a8c19692febabe24f8416e
王浩 <bingoogolapple@gmail.com>
2015-07-02 12:37:21 +0800

    update demo

--- app/src/main/java/cn/bingoogolapple/volleynote/engine/ApiClient.java
+++ app/src/main/java/cn/bingoogolapple/volleynote/engine/ApiClient.java
@@ -2,6 +2,11 @@ package cn.bingoogolapple.volleynote.engine;
 
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
+import cn.bingoogolapple.volleynote.model.Nest;
+import cn.bingoogolapple.volleynote.model.Normal;
+
 /**
  * 作者:王浩 邮件:bingoogolapple@gmail.com
  * 创建时间:15/7/2 11:27
@@ -9,23 +14,23 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class ApiClient {
     public static void testApiResponseNormal(AppCompatActivity activity, ApiResponseListener.ApiResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testApiResponseNormal.json", new ApiResponseListener<Normal>(activity, delegate));
     }
 
     public static void testApiResponseList(AppCompatActivity activity, ApiResponseListener.ApiResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testApiResponseList.json", new ApiResponseListener<ArrayList<Normal>>(activity, delegate));
     }
 
     public static void testApiResponseNest(AppCompatActivity activity, ApiResponseListener.ApiResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testApiResponseNest.json", new ApiResponseListener<Nest>(activity, delegate));
     }
 
     public static void testApiResponseNeedLogin(AppCompatActivity activity, ApiResponseListener.ApiResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testApiResponseNeedLogin.json", new ApiResponseListener<Normal>(activity, delegate));
     }
 
     public static void testApiResponseFailure(AppCompatActivity activity, ApiResponseListener.ApiResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testApiResponseFailure.json", new ApiResponseListener<Normal>(activity, delegate));
     }
 
     public static void testApiResponseJsonError(AppCompatActivity activity, ApiResponseListener.ApiResponseDelegate delegate) {
@@ -33,19 +38,18 @@ public class ApiClient {
     }
 
     public static void testGsonResponseNormal(AppCompatActivity activity, VolleyResponseListener.VolleyResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testGsonResponseNormal.json", new GsonResponseListener<Normal>(activity, delegate));
     }
 
     public static void testGsonResponseNest(AppCompatActivity activity, VolleyResponseListener.VolleyResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testGsonResponseNest.json", new GsonResponseListener<Nest>(activity, delegate));
     }
 
     public static void testGsonResponseList(AppCompatActivity activity, VolleyResponseListener.VolleyResponseDelegate delegate) {
-
+        RequestManager.get(activity, "https://raw.githubusercontent.com/bingoogolapple/VolleyNote/server/testGsonResponseList.json", new GsonResponseListener<ArrayList<Normal>>(activity, delegate));
     }
 
     public static void testGsonResponseJsonError(AppCompatActivity activity, VolleyResponseListener.VolleyResponseDelegate delegate) {
-
     }
 
 }
--- app/src/main/java/cn/bingoogolapple/volleynote/ui/activity/MainActivity.java
+++ app/src/main/java/cn/bingoogolapple/volleynote/ui/activity/MainActivity.java
@@ -4,7 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.android.volley.VolleyError;
+
+import java.util.ArrayList;
+
 import cn.bingoogolapple.volleynote.R;
+import cn.bingoogolapple.volleynote.engine.ApiClient;
+import cn.bingoogolapple.volleynote.engine.ApiResponseListener;
+import cn.bingoogolapple.volleynote.engine.VolleyResponseListener;
+import cn.bingoogolapple.volleynote.model.Nest;
+import cn.bingoogolapple.volleynote.model.Normal;
+import cn.bingoogolapple.volleynote.util.ToastUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,23 +25,148 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void testApiResponseNormal(View v) {
+        ApiClient.testApiResponseNormal(this, new ApiResponseListener.ApiResponseDelegate<Normal>() {
+            @Override
+            public void jumpToLogin() {
+                ToastUtil.show("请先登录");
+            }
 
+            @Override
+            public void onFailure(int errorCode, String errorDescription) {
+                ToastUtil.show(errorDescription);
+            }
+
+            @Override
+            public void onSucess(Normal content) {
+                ToastUtil.show("请求成功");
+            }
+
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testApiResponseList(View v) {
+        ApiClient.testApiResponseList(this, new ApiResponseListener.ApiResponseDelegate<ArrayList<Normal>>() {
+            @Override
+            public void onSucess(ArrayList<Normal> content) {
+                ToastUtil.show("请求成功");
+            }
 
+            @Override
+            public void jumpToLogin() {
+                ToastUtil.show("请先登录");
+            }
+
+            @Override
+            public void onFailure(int errorCode, String errorDescription) {
+                ToastUtil.show(errorDescription);
+            }
+
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testApiResponseNest(View v) {
+        ApiClient.testApiResponseNest(this, new ApiResponseListener.ApiResponseDelegate<Nest>() {
+            @Override
+            public void jumpToLogin() {
+                ToastUtil.show("请先登录");
+            }
 
+            @Override
+            public void onFailure(int errorCode, String errorDescription) {
+                ToastUtil.show(errorDescription);
+            }
+
+            @Override
+            public void onSucess(Nest content) {
+                ToastUtil.show("请求成功");
+            }
+
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testApiResponseNeedLogin(View v) {
+        ApiClient.testApiResponseNeedLogin(this, new ApiResponseListener.ApiResponseDelegate<Normal>() {
+            @Override
+            public void jumpToLogin() {
+                ToastUtil.show("请先登录");
+            }
 
+            @Override
+            public void onFailure(int errorCode, String errorDescription) {
+                ToastUtil.show(errorDescription);
+            }
+
+            @Override
+            public void onSucess(Normal content) {
+                ToastUtil.show("请求成功");
+            }
+
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testApiResponseFailure(View v) {
+        ApiClient.testApiResponseFailure(this, new ApiResponseListener.ApiResponseDelegate<Normal>() {
+            @Override
+            public void jumpToLogin() {
+                ToastUtil.show("请先登录");
+            }
 
+            @Override
+            public void onFailure(int errorCode, String errorDescription) {
+                ToastUtil.show(errorDescription);
+            }
+
+            @Override
+            public void onSucess(Normal content) {
+                ToastUtil.show("请求成功");
+            }
+
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testApiResponseJsonError(View v) {
@@ -39,15 +174,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void testGsonResponseNormal(View v) {
+        ApiClient.testGsonResponseNormal(this, new VolleyResponseListener.VolleyResponseDelegate<Normal>() {
+            @Override
+            public void onSucess(Normal content) {
+                ToastUtil.show("请求成功");
+            }
 
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testGsonResponseNest(View v) {
+        ApiClient.testGsonResponseNest(this, new VolleyResponseListener.VolleyResponseDelegate<Nest>() {
+            @Override
+            public void onSucess(Nest content) {
+                ToastUtil.show("请求成功");
+            }
 
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testGsonResponseList(View v) {
+        ApiClient.testGsonResponseList(this, new VolleyResponseListener.VolleyResponseDelegate<ArrayList<Normal>>() {
+            @Override
+            public void onSucess(ArrayList<Normal> content) {
+                ToastUtil.show("请求成功");
+            }
 
+            @Override
+            public void onJsonError(Exception e) {
+                ToastUtil.show("服务器异常");
+            }
+
+            @Override
+            public void onNetError(VolleyError error) {
+                ToastUtil.show("网络出错");
+            }
+        });
     }
 
     public void testGsonResponseJsonError(View v) {
