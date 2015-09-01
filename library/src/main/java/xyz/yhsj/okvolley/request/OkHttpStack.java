389859c4d18e8053891589607af196f6ac2220e4
永恒瞬间 <1130402124@qq.com>
2015-09-01 23:12:40 +0800

    修改了项目架构，暂时放入了okhttp的封装类

---
+++ library/src/main/java/xyz/yhsj/okvolley/request/OkHttpStack.java
@@ -0,0 +1,27 @@
+package xyz.yhsj.okvolley.request;
+
+import com.android.volley.toolbox.HurlStack;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:15/8/21 16:40
+ * 描述:
+ */
+public class OkHttpStack extends HurlStack {
+    private OkUrlFactory mOkUrlFactory;
+
+    public OkHttpStack(OkHttpClient okHttpClient) {
+        mOkUrlFactory = new OkUrlFactory(okHttpClient);
+    }
+
+    @Override
+    protected HttpURLConnection createConnection(URL url) throws IOException {
+        return mOkUrlFactory.open(url);
+    }
+}
